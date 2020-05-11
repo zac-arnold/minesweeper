f840d27e433ccb02d93d29eae94cbdc28d8b6e57
Zac Arnold <videozac@gmail.com>
2020-05-12 10:42:41 +1200

    initial commit plus starting code

--- minesweeper.js
+++ minesweeper.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-// var board = 
+var board = {
+  cells: [
+    {row: 0,
+      col: 0},
+    {row: 0,
+      col: 1},
+    {row: 0,
+      col: 2},
+    {row: 0,
+      col: 3},
+
+  ]
+
+}
+
 
 function startGame () {
   // Don't remove this function call: it makes the game work!
